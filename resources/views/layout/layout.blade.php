f930bc7714f659b20cbded9c5b0fcf78ba2639d5
Vilmantas Sudaris <vilmantas.sudaris@gmail.com>
2022-05-23 06:16:20 +0300

    upd: styles added

--- resources/views/layout/layout.blade.php
+++ resources/views/layout/layout.blade.php
@@ -1,14 +1,41 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/milligram/1.4.1/milligram.min.css" integrity="sha512-xiunq9hpKsIcz42zt0o2vCo34xV0j6Ny8hgEylN3XBglZDtTZ2nwnqF/Z/TTCc18sGdvCjbFInNd++6q3J0N6g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <span class="pln">
+        <!-- Google Fonts -->
+    </span>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,300italic,700,700italic"><span class="pln">
+
+        <!-- CSS Reset -->
+    </span>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.css"><span class="pln">
+
+        <!-- Milligram CSS -->
+    </span>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/milligram/1.4.1/milligram.css"><span class="pln">
+
+        <!-- You should properly set the path from the main file. -->
+    </span>
+    <link rel="stylesheet" href="/css/app.css">
+    <style>
+        body {
+            width: 60vw !important;
+            margin: 0 auto !important;
+        }
+    </style>
 </head>
+
 <body>
-    
+
     @yield('content')
 
 </body>
+
 </html>
